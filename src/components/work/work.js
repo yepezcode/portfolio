574e33272403ab997ae7800996c9bedfe26e02d2
Mauricio <yepezcode@gmail.com>
2021-03-24 13:19:52 -0700

    Arreglado el problema de los proyectos principales con grid

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -7,77 +7,70 @@ import Capture5 from '../../static/MernCalendar.png'
 import { CardWork } from './CardWork';
 export const Work = () => {
     return (
+        // <div className="work__container">
+        //     <h1 className="title__with-line"><span className="work__title">Proyects</span></h1>
+        //     <div className="work__content">
+        //         <div className="work__card">
+        //             <div className="work__images">
+        //                 <img src={Capture4} />
+        //             </div>
+        //             <div className="work__information">
+        //                 <p className="work__feature">Feature Project</p>
+        //                 <p className="work__work-title">Modest</p>
+        //                 <div className="work__work-information">
+        //                     <p >
+        //                         IA using bayesians networks for evaluate the competences of the students and create a view of skills. I win a  <a>Publication</a> in CIIE2019 and oportunitie of the present it in the Tecnologico de Monterrey campus Monterrey
+        //                     </p>
+        //                 </div>
+
+        //                 <ul className="work__technologies">
+        //                     <li>Python</li>
+        //                     <li>PySmile</li>
+        //                     <li>Arbor.js</li>
+        //                     <li>Django</li>
+
+        //                 </ul>
+        //             </div>
+
+        //         </div>
+               
+        //     </div>
+        // </div>
         <div className="work__container">
             <h1 className="title__with-line"><span className="work__title">Proyects</span></h1>
-            <div className="work__content">
-                <div className="work__card">
-                    <div className="work__images">
-                        <img src={Capture1} />
-                        <img src={Capture2} />
-                        <img src={Capture3} />
-                    </div>
-                    <div className="work__information">
-                        <p className="work__feature">Feature Project</p>
-                        <p className="work__work-title">Siina</p>
-                        <p className="work__work-information">
-                            Application for monitoring IoT devices using MQTT protocols, view state on real time and control and test devices using RTC </p>
-                        <ul className="work__technologies">
-                            <li>Expo</li>
-                            <li>Mqtt</li>
-                            <li>Python</li>
-                            <li>Express</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="work__card">
-                    <div className="work__images">
-                        <img src={Capture4} />
-                    </div>
-                    <div className="work__information">
-                        <p className="work__feature">Feature Project</p>
-                        <p className="work__work-title">Modest</p>
-                        <div className="work__work-information">
-                            <p >
-                                IA using bayesians networks for evaluate the competences of the students and create a view of skills. I win a  <a>Publication</a> in CIIE2019 and oportunitie of the present it in the Tecnologico de Monterrey campus Monterrey
-                            </p>
+            <div className="work__card-container">
+                <div className="work__text">
+                    <div className="grid_title">
+                        <div className="work_feature">
+                            <p className="work__feature">Feature Project</p>
                         </div>
-
-                        <ul className="work__technologies">
-                            <li>Python</li>
-                            <li>PySmile</li>
-                            <li>Arbor.js</li>
-                            <li>Django</li>
-
-                        </ul>
-                    </div>
-
-                </div>
-                <div className="work__card">
-                    <div className="work__images">
-                        <img style={{   marginTop: "100px",
-                        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-                        height: "100"
-                     }} src={Capture5} />
-                    </div>
-                    <div className="work__information">
-                        <p className="work__feature">Feature Project</p>
-                        <p className="work__work-title">MERN Calendar</p>
-                        <div className="work__work-information">
-                            <p >
-                            Calendar for create reminders, create account and view all recordatories for others users.
-                            </p>
+                        <div className="work_name-title">
+                             <p className="work__work-title">Modest</p>
                         </div>
-                        <ul className="work__technologies">
-                            <li>MongoDB</li>
-                            <li>Express</li>
-                            <li>React</li>
-                            <li>Redux </li>
-                            <li>NodeJS</li>
-                        </ul>
                     </div>
-
+                    <div className="grid_information">
+                        <div className="work_information">
+                        <p >
+                            IA using bayesians networks for evaluate the competences of the students and create a view of skills. I win a  <a>Publication</a> in CIIE2019 and oportunitie of the present it in the Tecnologico de Monterrey campus Monterrey
+                        </p>
+                        </div>
+                        <div className="work_technologies">
+                            <ul className="work__technologies">
+                                <li>Python</li>
+                                <li>PySmile</li>
+                                <li>Arbor.js</li>
+                                <li>Django</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
+                <div className="work__imagen">
+                    <img src={Capture4} />
+                </div>
+
             </div>
+            
         </div>
+        
     )
 }

a5a2535193517ef8ba89c7a0c3d2537f50af2d68
Mauricio <yepezcode@gmail.com>
2021-03-24 14:58:52 -0700

    Disenio terminado

--- src/components/work/CardWork.js
+++ src/components/work/CardWork.js
@@ -3,31 +3,39 @@ import React from 'react'
 // Este es un componente para no hacer demasiado codigo, sin embargo, al querer personalizar texto del codigo
 // no me deja lo detecta como texto. 
 
-export const CardWork = ({ images, nameProject, information, technologies, imagenSize = 300 }) => {
-    console.log(information);
+export const CardWork = ({ images, nameProject, technologies, imagenSize = 300, children }) => {
     return (
-        <div className="work__card">
-            <div className="work__images" >
-               {
-                   images.map( imagen => (
-                       <img style={{ height: imagenSize }} src={imagen} />
-                   ))
-               }
+        <div className="work__card-container">
+                <div className="work__text">
+                    <div className="grid_title">
+                        <div className="work_feature">
+                            <p className="work__feature">Feature Project</p>
+                        </div>
+                        <div className="work_name-title">
+                             <p className="work__work-title">{ nameProject }</p>
+                        </div>
+                    </div>
+                    <div className="grid_information">
+                        <div className="work_information">
+                        <p >
+                            { children }
+                        </p>
+                        </div>
+                        <div className="work_technologies">
+                            <ul className="work__technologies">
+                                {
+                                    technologies.map( tecnologie => (
+                                        <li>{tecnologie}</li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="work__imagen">
+                    <img src={images} />
+                </div>
+
             </div>
-            <div className="work__information">
-                <p className="work__feature">Feature Project</p>
-                <p className="work__work-title">{ nameProject }</p>
-                <p className="work__work-information">
-                   { information } 
-                </p>
-                <ul className="work__technologies">
-                    {
-                        technologies.map( (techonologie) => (
-                            <li>{ techonologie }</li>
-                        ))
-                    }
-                </ul>
-            </div>
-        </div>
     )
 }
--- src/components/work/work.js
+++ src/components/work/work.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Capture1 from '../../static/Captura.png';
-import Capture2 from '../../static/capture2.png';
-import Capture3 from '../../static/Captura3.png';
-import Capture4 from '../../static/Modest.png';
-import Capture5 from '../../static/MernCalendar.png'
+import Capture1 from '../../static/Siina.png';
+import Capture2 from '../../static/Modest.png';
+import Capture3 from '../../static/MernCalendar.png'
 import { CardWork } from './CardWork';
 export const Work = () => {
     return (
@@ -38,37 +36,32 @@ export const Work = () => {
         // </div>
         <div className="work__container">
             <h1 className="title__with-line"><span className="work__title">Proyects</span></h1>
-            <div className="work__card-container">
-                <div className="work__text">
-                    <div className="grid_title">
-                        <div className="work_feature">
-                            <p className="work__feature">Feature Project</p>
-                        </div>
-                        <div className="work_name-title">
-                             <p className="work__work-title">Modest</p>
-                        </div>
-                    </div>
-                    <div className="grid_information">
-                        <div className="work_information">
-                        <p >
-                            IA using bayesians networks for evaluate the competences of the students and create a view of skills. I win a  <a>Publication</a> in CIIE2019 and oportunitie of the present it in the Tecnologico de Monterrey campus Monterrey
-                        </p>
-                        </div>
-                        <div className="work_technologies">
-                            <ul className="work__technologies">
-                                <li>Python</li>
-                                <li>PySmile</li>
-                                <li>Arbor.js</li>
-                                <li>Django</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className="work__imagen">
-                    <img src={Capture4} />
-                </div>
+            
+            <CardWork
+                images={ Capture1 }
+                nameProject="Siina"
+                technologies={["Expo","MQTT","Python","Express"]}
+            >
+                Application for monitoring IoT devices using MQTT protocols, view state on real time and control and test devices using RTC 
+            </CardWork>
+            <CardWork
+                 images={ Capture2 }
+                 nameProject="Modest"
+                 technologies={["Python","PySmile","Arbor.js","Django"]}
+            >
+                 IA using bayesians networks for evaluate the competences of the students and create a view of skills. I win a  <a>Publication</a> in CIIE2019 and oportunitie of the present it in the Tecnologico de Monterrey campus Monterrey
+            </CardWork>
 
-            </div>
+            <CardWork
+                 images={ Capture3 }
+                 nameProject="MERN Calendar"
+                 technologies={["MongoDB","Express","React","NodeJS", "Redux"]}
+            >
+                Calendar for create reminders, create account and view all recordatories for others users
+
+            </CardWork>
+
+        
             
         </div>
         

da9f14df19cca1ef04922baab0a9e9319f7a68b4
Mauricio Yepez <yepezcode@gmail.com>
2021-03-26 11:02:24 -0600

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {PortfolioApp} from './PortfolioApp';
-import './styles/styles.scss';
-ReactDOM.render(
-    <PortfolioApp />,
-  document.getElementById('root')
-);
